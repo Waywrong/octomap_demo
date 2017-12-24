3b61953e6b5acd29874f3dde7a3dcf8529151f61
Waywrong <wlhsu@ira.ee.ntu.edu.tw>
2017-12-24 23:23:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+octomap_test
+============
+
+### pub_pcl demo
+```
+rosrun octomap_test pub_pcl 
+roslaunch octomap_test octomap_server_test.launch
+```
+```
+http://blog.csdn.net/crp997576280/article/details/74605766
+```
---
+++ src/pub_pcl.cpp
@@ -0,0 +1,29 @@
+#include<ros/ros.h>  
+#include<pcl/point_cloud.h>  
+#include<pcl_conversions/pcl_conversions.h>  
+#include<sensor_msgs/PointCloud2.h>  
+#include<pcl/io/pcd_io.h>
+
+int main (int argc, char **argv)  
+{  
+  ros::init (argc, argv, "pub_pcl");  
+  ros::NodeHandle nh;  
+  ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("/pub_pcl/output", 10);  
+
+  pcl::PointCloud<pcl::PointXYZ> cloud;  
+  sensor_msgs::PointCloud2 output;  
+  pcl::io::loadPCDFile("/home/peter/code/ira_slam/src/octomap_test/data/office/office1.pcd", cloud);  
+  pcl::toROSMsg(cloud,output);
+
+  output.header.stamp=ros::Time::now();
+  output.header.frame_id  ="camera_rgb_frame";
+
+  ros::Rate loop_rate(1);  
+  while (ros::ok())  
+  {  
+    pcl_pub.publish(output);  
+    ros::spinOnce();  
+    loop_rate.sleep();  
+  }  
+  return 0;  
+}  
